97957fcbef7dea3b046e8e15066e5b871a77a88b
Mike <131572519+DMike20@users.noreply.github.com>
2024-05-27 12:01:25 +0800

    Add files via upload

--- blynknotif.py
+++ blynknotif.py
@@ -1,26 +1,43 @@
 import BlynkLib
 import time
+from BlynkTimer import BlynkTimer
 
 # Initialize Blynk
-BLYNK_AUTH = 'YourAuthToken'
+BLYNK_AUTH = 'XIxKMdtkdcxn6RQUYeDRXqD8afPswzY3'
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
-prediction = "tired"  # Change this value to test different conditions
+
+# Create BlynkTimer Instance
+timer = BlynkTimer()
+
+# Function to sync the data from virtual pins
+@blynk.on("connected")
+def blynk_connected():
+    print("Hi, You have Connected to New Blynk2.0")
+    print(".......................................................")
+    time.sleep(2)
+prediction = "burping"  
 
 def send_blynk_notification(message):
-    blynk.notify(message)
+    blynk.log_event(message)
 
 # Function to handle different predictions
 def handle_prediction(prediction):
     if prediction == "tired":
-        send_blynk_notification("The baby is tired")
+        print("The baby is tired")
+        blynk.log_event("tired","The baby is tired")
     elif prediction == "hungry":
-        send_blynk_notification("The baby is hungry")
+        print("The baby is hungry")
+        blynk.log_event("hungry","The baby is hungry")
     elif prediction == "discomfort":
-        send_blynk_notification("The baby is in discomfort")
+        print("The baby is in discomfort")
+        blynk.log_event("discomfot","The baby is in discomfort")
     elif prediction == "burping":
-        send_blynk_notification("The baby needs burping")
+        print("The baby needs burping")
+        blynk.log_event("burping","The baby needs burping")
     elif prediction == "belly pain":
-        send_blynk_notification("The baby has belly pain")
+        print("The baby has belly pain")
+        blynk.log_event("belly pain","The baby has belly pain")
+        
 
 if __name__ == "__main__":
     # Connect to Blynk server and send the notification based on the prediction
